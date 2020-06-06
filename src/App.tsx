689f64fdc9ade71c2d507336916f27376c17b815
CoderX <syedhaiderabbas6@gmail.com>
2020-06-06 15:37:27 +0500

    ReactGA

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import WorldStats from './components/WorldStats';
 import CountryStats from './components/CountryStats';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ReactGA from 'react-ga';
 
+const trackingId = "UA-161713714-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.pageview('/');
 const App: FC = () => {
   return (
     <div className="min-h-screen p-4 md:flex md:justify-center md:items-center">
