a9e1ed82764bb29472b91a71dcb669b1b4e22f46
CoderX <syedhaiderabbas6@gmail.com>
2020-06-06 14:49:48 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,12 @@ const Footer: FC = () => {
     return (
         <div className="mt-2 text-xs text-center Footer text-muted">
             <div className="flex flex-col items-center justify-center mt-2 md:flex-row">
-                <div>{t('provided-by')} <a className="underline hover:no-underline" href="https://github.com/" target="_blank" rel="noopener noreferrer">COVID-19 API</a></div>
+                <div>{t('provided-by')} <a className="underline hover:no-underline" href="#" target="_blank" rel="noopener noreferrer">COVID-19 API</a></div>
                 <div className="hidden px-1 md:inline-block">|</div>
                 <div>
                     {t('powered-by')} <a className="underline hover:no-underline" href="https://www.facebook.com/coderx" target="_blank" rel="noopener noreferrer">Haider Abbas</a>
                     <div className="inline-block px-1 md:hidden">|</div>
-                    <a className="inline-block underline md:hidden hover:no-underline" href="https://github.com/cdmoro/covid-19-stats" target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a className="inline-block underline md:hidden hover:no-underline" href="https://github.com/syed-haider5/covid" target="_blank" rel="noopener noreferrer">GitHub</a>
                 </div>
             </div>
         </div>
