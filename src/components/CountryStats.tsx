627516018dce359bf97329644020174d02319659
S. Haider Abbas <muhammad.anees@lums.edu.pk>
2020-06-07 01:14:34 +0500

    function

--- src/components/CountryStats.tsx
+++ src/components/CountryStats.tsx
@@ -52,6 +52,7 @@ interface ICountryData {
 type ICountryRest = Record<ICountry['iso2'], ICountryData>
 
 const CountryStats: FC = () => {
+    getUserCurrentCountry();
     const { t } = useTranslation()
     const [selectedCountry, setSelectedCountry] = useLocalStorage('country-selected', COUNTRY_DEFAULT);
     const [countriesData, setCountriesData] = useState<ICountryRest>({})
